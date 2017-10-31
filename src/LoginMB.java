6ef2cd86a7a9620e3f25701d19c59239a4ae6da1
iurysantana <Iury@DESKTOP-VBDAF8L>
2017-10-31 20:11:56 -0200

    COMITANDO COM ECLIPSE

---
+++ src/LoginMB.java
@@ -0,0 +1,25 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LoginMB {
+  private Usuario usuario = new Usuario();
+
+  public String doEfetuarLogin() {
+    if("admin".equals(usuario.getLogin()) &&
+       "123".equals(usuario.getSenha())) {
+      /* Se escrever o login e senha correto então vai para a tela principal do sistema. */
+      return "principal";
+    }
+
+    //Caso erre o login ou senha fica na mesma tela.
+    return null;
+  }
+
+  public Usuario getUsuario() {
+    return usuario;
+  }
+
+  public void setUsuario(Usuario usuario) {
+    this.usuario = usuario;
+  }
+}
